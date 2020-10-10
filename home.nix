e9b8082f3a26bdc22164b09fd9fc6c6fc8b495d2
loooph <loooph@gmx.de>
2020-10-10 23:16:35 +0200

    added language related settings

--- home.nix
+++ home.nix
@@ -21,4 +21,24 @@ in
 
   programs.home-manager.enable = true;
 
+  home = {
+    keyboard = {
+      layout = "de";
+    };
+    language = {
+      base = "en_US.UTF-8";
+      address = "de_DE.UTF-8";
+      collate = "de_DE.UTF-8";
+      ctype = "de_DE.UTF-8";
+      measurement = "de_DE.UTF-8";
+      messages = "en_US.UTF-8";
+      monetary = "de_DE.UTF-8";
+      name = "de_DE.UTF-8";
+      numeric = "en_US.UTF-8";
+      paper = "de_DE.UTF-8";
+      telephone = "de_DE.UTF-8";
+      time = "de_DE.UTF-8";
+    };
+  };
+
 }
